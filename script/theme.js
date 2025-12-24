0b4256d78f03a986ddca35d55f64b14b19496a02
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-24 14:27:06 +0600

    addad theme color in thos file

---
+++ script/theme.js
@@ -0,0 +1,4 @@
+document.getElementById("theme-btn").addEventListener("click", function () {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.background = randomColor;
+});
