57a9382d83bc3fced85bd90318b97d86e441b18e
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-25 23:41:56 +0600

    addad addevent listener click to add number

--- script/toggle.js
+++ script/toggle.js
@@ -1,6 +1,11 @@
-const btn = document.getElementById("first-btn");
-
-btn.addEventListener("click", function () {
-  alert("Fix Mobile Button Issue is successful");
-  btn.disabled = true;
-});
+const btn = document
+  .getElementById("first-btn")
+  .addEventListener("click", function () {
+    const number = document.getElementById("numbers").innerText;
+    const convertNumber = parseInt(number);
+    const num = 1;
+    const result = convertNumber + num;
+    document.getElementById("numbers").innerText = result;
+    alert("Fix Mobile Button Issue is successful");
+  });
+btn.disabled = true;
