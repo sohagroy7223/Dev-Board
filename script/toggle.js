e00ca7665385a65ae5c158dfc10b0ace6614d23c
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-26 11:03:00 +0600

    addad utilities function whene we call the function anywhere it is run

--- script/toggle.js
+++ script/toggle.js
@@ -1,21 +1,10 @@
 const btn = document.getElementById("first-btn");
 btn.addEventListener("click", function () {
-  const number = document.getElementById("numbers").innerText;
-  const convertNumber = parseInt(number);
-  const num = 1;
-  const result = convertNumber + num;
-  document.getElementById("numbers").innerText = result;
+  btnClickToAddNumber("numbers");
   btn.disabled = true;
-  const numb = document.getElementById("number").innerText;
-  const convertNumbers = parseInt(numb);
-  const nums = 1;
-  const results = convertNumbers - nums;
-  document.getElementById("number").innerText = "0" + results;
 
-  const history = document.getElementById("history");
-  const p = document.createElement("p");
-  p.innerText =
-    "you have complete the task Fix Mobile Button Issue is successful ";
-  history.appendChild(p);
+  btnClickToOddNumber("number");
+  activityLogOutput("history");
+
   alert("Fix Mobile Button Issue is successful");
 });
---
+++ utilities/utilities.js
@@ -0,0 +1,21 @@
+function btnClickToAddNumber(id) {
+  const textValue = document.getElementById(id).innerText;
+  const convertTextValue = parseInt(textValue);
+  const numb = 1;
+  const result = convertTextValue + numb;
+  document.getElementById(id).innerText = result;
+}
+function btnClickToOddNumber(id) {
+  const textValues = document.getElementById(id).innerText;
+  const convertTextValues = parseInt(textValues);
+  const num = 1;
+  const results = convertTextValues - num;
+  document.getElementById(id).innerText = "0" + results;
+}
+function activityLogOutput(id) {
+  const history = document.getElementById(id);
+  const p = document.createElement("p");
+  p.innerText =
+    "you have complete the task Fix Mobile Button Issue is successful ";
+  history.appendChild(p);
+}
